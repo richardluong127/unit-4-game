82424cfd576752da7a6cb78958599ea65788e204
richardluong127 <richardluong127@gmail.com>
2018-10-11 00:17:16 -0700

    setting up javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+
+window.onload = function() {
+
+$("#crystal1").click(randomNumber.crystal1);
+$("#crystal2").click(randomNumber.crystal2);
+$("#crystal3").click(randomNumber.crystal3);
+$("#crystal4").click(randomNumber.crystal4);
+
+var crystal1 = 0;
+var crystal2 = 0;
+var crystal3 = 0;
+var crystal4 = 0;
+
+$("#randomGenerated").text(Math.floor(Math.random()*100+19));
+this.console.log();
+
+
+
+}
