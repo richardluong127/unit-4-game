6f4d786f17e0a3bb163a6aaa2ad406bb34585382
richardluong127 <richardluong127@gmail.com>
2018-10-11 00:58:54 -0700

    random generators set up xD

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,23 @@
 
 window.onload = function() {
 
-$("#crystal1").click(randomNumber.crystal1);
-$("#crystal2").click(randomNumber.crystal2);
-$("#crystal3").click(randomNumber.crystal3);
-$("#crystal4").click(randomNumber.crystal4);
+//("#crystal1").click(randomNumber.crystal1);
+//("#crystal2").click(randomNumber.crystal2);
+//("#crystal3").click(randomNumber.crystal3);
+//("#crystal4").click(randomNumber.crystal4);
 
 var crystal1 = 0;
 var crystal2 = 0;
 var crystal3 = 0;
 var crystal4 = 0;
 
-$("#randomGenerated").text(Math.floor(Math.random()*100+19));
-this.console.log();
+var randomGen=
+    $("#randomGenerated").text(Math.floor(Math.random()*100+19));
+// how to console log?
 
-
+var crystalRandomNumber1=$("#crystal1").text(Math.floor(Math.random()*12+1));
+var crystalRandomNumber2=$("#crystal2").text(Math.floor(Math.random()*12+1));
+var crystalRandomNumber3=$("#crystal3").text(Math.floor(Math.random()*12+1));
+var crystalRandomNumber4=$("#crystal4").text(Math.floor(Math.random()*12+1));
 
 }
