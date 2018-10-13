57fd4e58230566e5d3b1f052a639e506ddc9be6b
richardluong127 <richardluong127@gmail.com>
2018-10-13 14:51:57 -0700

    crystals add properly

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # unit-4-game
+
+Game starts when a crystal is clicked.
+Each crystal generates a random number.
+Target number randomly generates.
+
+Each crystal number stays until the game is won or lost, then resets.
+
+Come up with a function:
+If currentSum === randomGen, +1 win
+If currentSum > randomGen, +1 Loss
+If currentSum < randomGen, add the clicked number to the currentSum.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,71 @@
 
-window.onload = function() {
+//window.onload = function() {
+$(document).ready(function () {
+    //("#crystal1").click(randomNumber.crystal1);
+    //("#crystal2").click(randomNumber.crystal2);
+    //("#crystal3").click(randomNumber.crystal3);
+    //("#crystal4").click(randomNumber.crystal4);
+    // or try .append
+    var crystal1 = (Math.floor(Math.random() * 12 + 1));
+    var crystal2 = (Math.floor(Math.random() * 12 + 1));
+    var crystal3 = (Math.floor(Math.random() * 12 + 1));
+    var crystal4 = (Math.floor(Math.random() * 12 + 1));
+    //var crystalNumbers = [crystalStart1, crystalStart2, crystalStart3, crystalStart4];
 
-//("#crystal1").click(randomNumber.crystal1);
-//("#crystal2").click(randomNumber.crystal2);
-//("#crystal3").click(randomNumber.crystal3);
-//("#crystal4").click(randomNumber.crystal4);
+    //var numbersMatch = false;
+    //var crystalPressed = false;
+    //var changeNumberWhenPressed = false;
+    var randomGen = Math.floor(Math.random() * 100 + 19);
+    $("#randomGenerated").text(randomGen);
+    console.log(randomGen);
 
-var crystal1 = 0;
-var crystal2 = 0;
-var crystal3 = 0;
-var crystal4 = 0;
+    //var totalSum = 0;
+    var currentSum = 0;
+    var wins = "";
+    var losses = "";
+    // how to console log?
 
-var randomGen=
-    $("#randomGenerated").text(Math.floor(Math.random()*100+19));
-// how to console log?
 
-var crystalRandomNumber1=$("#crystal1").text(Math.floor(Math.random()*12+1));
-var crystalRandomNumber2=$("#crystal2").text(Math.floor(Math.random()*12+1));
-var crystalRandomNumber3=$("#crystal3").text(Math.floor(Math.random()*12+1));
-var crystalRandomNumber4=$("#crystal4").text(Math.floor(Math.random()*12+1));
+    //var crystalRandomNumber=(Math.floor(Math.random()*12+1));
 
-}
+    //$(".crystal").on("click", function() {
+    //    var crystalRandomNumber=0+(Math.floor(Math.random()*12+1));
+    //    console.log(crystalRandomNumber);
+    //});
+    //event.target ---- look up
+
+    $(".crystal").on("click", function (event) {
+        var numbersMatch = false;
+        console.log(event.target.id);
+        if (event.target.id === "crystal1") {
+            currentSum += crystal1;
+            console.log(currentSum);
+        }if (event.target.id === "crystal2") {
+            currentSum += crystal2;
+            console.log(currentSum);
+        }if (event.target.id === "crystal3") {
+            currentSum += crystal3;
+            console.log(currentSum);
+        }if (event.target.id === "crystal4") {
+            currentSum += crystal4;
+            console.log(currentSum);
+        }$("#numbersAdded").text(currentSum);
+
+
+    });
+
+    // if (currentSum === randomGen) {
+    //     wins++;
+    //     $("#wins").text(wins);
+    // } if (totalSum > randomGen) {
+    //     losses++;
+    //     $("#losses").text(losses);
+    // } if (totalSum < randomGen) {
+    //     $(".crystal").on("click", { x: i }, function () {
+    //         currentSum = totalSum + indexOf(crystalArray);
+    //         $("#yourScore").text(currentSum);
+    //     });
+    // };
+
+});
+
